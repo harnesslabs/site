d3620eb9a5786c44ce85da6a9fd540adcccd4f86
Parker Smith <parker@getmagical.com>
2025-05-25 16:06:46 -0600

    Ignore wasm in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +14,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   // Prettier integration - must be last to override other formatting rules
   ...compat.extends("prettier"),
+  globalIgnores(["src/playground-wasm/**/*"]),
 ];
 
 export default eslintConfig;
