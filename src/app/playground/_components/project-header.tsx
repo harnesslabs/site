4e5a81baa21875124d5cb2e213a3f639ee73b810
Parker Smith <parker@getmagical.com>
2025-05-25 18:03:19 -0600

    Make navbar sticky and make it work on mobile

--- src/app/playground/_components/project-header.tsx
+++ src/app/playground/_components/project-header.tsx
@@ -3,7 +3,7 @@ import { Project } from "@/data";
 export function ProjectHeader({ project }: { project: Project }) {
   return (
     <div className="flex flex-col gap-2 items-center justify-center">
-      <h1 className="text-sm text-muted-foreground uppercase tracking-wider mb-2">
+      <h1 className="lg:block hidden text-sm text-muted-foreground uppercase tracking-wider mb-2">
         {project.name}
       </h1>
       <p className="text-md max-w-2xl text-center">{project.description}</p>
--- src/app/playground/layout.tsx
+++ src/app/playground/layout.tsx
@@ -12,11 +12,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex flex-grow flex-row text-foreground py-20 px-4 sm:px-6 lg:px-8 h-full">
-      <div className="flex-grow flex">
-        <NavBar />
+    <main className="flex flex-grow lg:grid-cols-[200px_minmax(900px,_1fr)_100px] lg:grid flex-col gap-4 lg:gap-0 text-foreground py-20 px-4 sm:px-6 lg:px-8 h-full">
+      {/* magic number 85px is the height of the navbar */}
+      <div className="lg:flex-grow flex h-full items-center">
+        <div className="lg:fixed lg:top-[calc(50%-85px)] lg:-translate-y-1/2 flex items-center flex-grow">
+          <NavBar />
+        </div>
       </div>
-      <div className="w-full space-y-16">{children}</div>
+      <div className="w-full">{children}</div>
     </main>
   );
 }
