72ffc7731383b1ca8726880346864e529e759abe
Parker Smith <parker@getmagical.com>
2025-05-25 18:03:19 -0600

    Make navbar sticky and make it work on mobile

--- src/app/playground/_components/navbar.tsx
+++ src/app/playground/_components/navbar.tsx
@@ -9,13 +9,13 @@ export function NavBar() {
   const pathname = usePathname();
 
   return (
-    <div className="flex justify-center items-starts flex-grow flex-col gap-2">
+    <div className="flex justify-center items-starts flex-grow flex-row lg:flex-col gap-2">
       {projects.map((project) => (
         <Link
           key={project.slug}
           className={cn(
             "text-md text-muted-foreground uppercase tracking-wider text-nowrap",
-            pathname === `/playground/${project.slug}` && "text-foreground text-xl font-bold"
+            pathname === `/playground/${project.slug}` && "text-foreground lg:text-xl font-bold"
           )}
           href={`/playground/${project.slug}`}
         >
